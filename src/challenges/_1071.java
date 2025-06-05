cee81cd7694d0657a5f442319f5a1202deafe921
marcospaulor <dev.silva.marcos@gmail.com>
2025-06-04 22:59:11 -0300

    feat: gdc0fStrings implementation

--- src/challenges/_1071.java
+++ src/challenges/_1071.java
@@ -4,11 +4,28 @@ import interfaces.ChallengesInterface;
 
 import java.util.Scanner;
 
+
 public class _1071 implements ChallengesInterface {
+
+    String gdc0fStrings(String str1, String str2) {
+        if(!(str1 + str2).equals(str2 + str1)){
+            return "";
+        }
+
+        int sizeA = str1.length(), sizeB = str2.length();
+        while (sizeB != 0){
+            int temp = sizeB;
+            sizeB = sizeA % sizeB;
+            sizeA = temp;
+        }
+
+        return str1.substring(0, sizeA);
+    }
 
     @Override
     public void solve(Scanner scanner) {
-        System.out.println("Solved");
+        String result = this.gdc0fStrings(scanner.nextLine(), scanner.nextLine());
+        System.out.println(result);
     }
 
     @Override
